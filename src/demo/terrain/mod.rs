458b79f74353bd0cf6647c2830c69160a972044f
Julian Bieber <j.bieber@omniaretail.com>
2025-11-02 12:16:27 +0100

    init

---
+++ src/demo/terrain/mod.rs
@@ -0,0 +1,12 @@
+use bevy::prelude::*;
+use noiz::prelude::*;
+
+pub struct TerrainPlugin;
+
+impl Plugin for TerrainPlugin {
+    fn build(&self, app: &mut App) {
+        todo!()
+    }
+}
+
+// oceans will be
