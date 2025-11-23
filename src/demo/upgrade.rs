652afc3de047aa121795531fe016b86eb104cccc
Julian Bieber <j.bieber@omniaretail.com>
2025-11-24 00:23:43 +0100

    upgrade ui setup

--- src/demo/upgrade.rs
+++ src/demo/upgrade.rs
@@ -1,6 +1,9 @@
 use bevy::prelude::*;
 
-use crate::{screens::Screen, theme::widget::label};
+use crate::{
+    screens::Screen,
+    theme::widget::{button, label},
+};
 
 pub struct UpgradePlugin;
 
@@ -11,7 +14,7 @@ impl Plugin for UpgradePlugin {
 }
 
 fn setup_upgrade_ui(mut commands: Commands) {
-    commands.spawn((block_root(), children![column(), column()]));
+    commands.spawn((block_root(), children![weapons_column(), stats_column()]));
 }
 
 fn block_root() -> impl Bundle {
@@ -22,26 +25,90 @@ fn block_root() -> impl Bundle {
             display: Display::Grid,
             width: percent(100),
             height: percent(100),
-            grid_template_columns: vec![GridTrack::min_content(), GridTrack::flex(1.0)],
+            grid_template_columns: vec![GridTrack::flex(1.0), GridTrack::flex(1.0)],
             ..default()
         },
         // Don't block picking events for other UI roots.
         Pickable::IGNORE,
     )
 }
-pub fn column() -> impl Bundle {
+pub fn weapons_column() -> impl Bundle {
     (
         Node {
             position_type: PositionType::Relative,
-            width: percent(45),
+            width: percent(100),
             height: percent(100),
             align_items: AlignItems::Center,
             flex_direction: FlexDirection::Column,
             justify_content: JustifyContent::Center,
-            display: Display::Grid,
+            display: Display::Flex,
             ..Default::default()
         },
         Pickable::IGNORE,
-        children![(label("AAAAAAAAAAAAAAAAA"))],
+        children![
+            (label("Weapons")),
+            button("Top", tmp_click),
+            (
+                Node {
+                    position_type: PositionType::Relative,
+                    width: percent(100),
+                    height: percent(100),
+                    // align_items: AlignItems::Center,
+                    justify_content: JustifyContent::Center,
+                    display: Display::Grid,
+                    grid_template_columns: vec![GridTrack::flex(1.0), GridTrack::flex(1.0)],
+                    ..Default::default()
+                },
+                children![
+                    (
+                        Node {
+                            flex_direction: FlexDirection::Column,
+                            ..Default::default()
+                        },
+                        children![
+                            button("left_top", tmp_click),
+                            button("left_middle", tmp_click),
+                            button("left_bottom", tmp_click),
+                        ]
+                    ),
+                    (
+                        Node {
+                            flex_direction: FlexDirection::Column,
+                            ..Default::default()
+                        },
+                        children![
+                            button("right_top", tmp_click),
+                            button("right_middle", tmp_click),
+                            button("right_bottom", tmp_click)
+                        ]
+                    )
+                ]
+            ),
+        ],
     )
+}
+pub fn stats_column() -> impl Bundle {
+    (
+        Node {
+            position_type: PositionType::Relative,
+            width: percent(100),
+            height: percent(100),
+            align_items: AlignItems::Center,
+            flex_direction: FlexDirection::Column,
+            justify_content: JustifyContent::Center,
+            display: Display::Flex,
+            ..Default::default()
+        },
+        Pickable::IGNORE,
+        children![
+            (label("Stats")),
+            button("Stat1", tmp_click),
+            button("Stat2", tmp_click),
+            button("Stat3", tmp_click),
+        ],
+    )
+}
+
+fn tmp_click(_: On<Pointer<Click>>) {
+    warn!("click");
 }
